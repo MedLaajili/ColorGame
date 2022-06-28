f8b780fe9236bc26f5f6e5b51ae0d0b06cbb18ac
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-06-28 13:20:56 +0100

    Project Initiation

---
+++ src/main/java/com/Laajili/ColorGame/ColorGameApplication.java
@@ -0,0 +1,13 @@
+package com.Laajili.ColorGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorGameApplication.class, args);
+	}
+
+}
