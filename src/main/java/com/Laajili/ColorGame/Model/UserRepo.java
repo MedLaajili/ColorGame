316f6211922eb101ed7aacab068ca73689d55836
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-06-29 13:54:41 +0100

    Adding findByUsername Method to UserRepo interface

--- src/main/java/com/Laajili/ColorGame/Model/UserRepo.java
+++ src/main/java/com/Laajili/ColorGame/Model/UserRepo.java
@@ -3,4 +3,5 @@ package com.Laajili.ColorGame.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
